9a33aafdd0fc8ca400401f282df8d1c1f7ee3d1d
BlaKe00786 <sameershaikh403@gmail.com>
2020-11-06 19:30:01 +0530

    [Sameer]Add: Establish Cooncetion with database.

---
+++ PayrollServiceProject/EmployeeRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace PayrollServiceProject
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=LAPTOP-76GI0P4R\\SQLEXPRESS;Initial Catalog=testDB;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void checkConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                Console.WriteLine("Connection established");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
